e89c1cc2a29620a790ebab0c60a7c062fe277f84
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-09-28 15:26:48 +1000

    Add files via upload

---
+++ Variable Scope.cpp
@@ -0,0 +1,26 @@
+﻿// Variable Scope.cpp : Defines the entry point for the application.
+//
+
+#include "Variable Scope.h"
+#include "string"
+#include "Windows.h"
+#include "iostream"
+
+using namespace std;
+
+// global variable
+int globalN = 144;
+
+
+int main()
+{
+	// local variables 
+	int localN = 72;
+
+	cout << "\n " << "# of data points (LOCAL) = " << localN;
+	cout << "\n " << "# of data points (GLOBAL) = " << globalN;
+
+	cin.get(); // Pause console window 
+
+	return 0;
+}
