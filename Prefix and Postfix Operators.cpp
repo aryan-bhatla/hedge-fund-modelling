8a197ddd336777d6be72a9d48e032764c3fc7743
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-09-28 15:27:17 +1000

    Add files via upload

---
+++ Prefix and Postfix Operators.cpp
@@ -0,0 +1,32 @@
+﻿// Prefix and Postfix Operators.cpp : Defines the entry point for the application.
+//
+
+#include "Prefix and Postfix Operators.h"
+#include "Windows.h"
+#include "string"
+
+using namespace std;
+
+int main()
+{
+	// declare variable 
+	int y = 3; 
+
+
+	// postfix
+	cout << "\n " << "Postfix y++" << "\n ";
+	cout << "x = " << y++ << "\n ";
+	cout << "y = " << y << "\n ";
+
+	// reset y
+	y = 3;
+
+	// prefix
+	cout << "\n " << "Prefix ++y" << "\n ";
+	cout << "x = " << ++y << "\n ";
+	cout << "y = " << y << "\n\n ";
+	
+	cin.get(); // pause console window
+
+	return 0;
+}
