8e64b6978433cad9644c60753098f0afd8d41707
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-09-28 15:29:15 +1000

    Add files via upload

---
+++ If Else Statement.cpp
@@ -0,0 +1,27 @@
+﻿// If Else Statement.cpp : Defines the entry point for the application.
+//
+
+#include "If Else Statement.h"
+#include "windows.h"
+#include "string.h"
+
+using namespace std;
+
+int main()
+{
+	int x; 
+
+	cout << "\n " << "Enter a number: ";
+	cin >> x; 
+
+	// If else statement 
+	if (x > 100)
+	{
+		cout << "\n" << "Number greater than 100" << "\n";
+	}
+	else
+	{
+		cout << " " << "Number less than 100" << "\n";
+	}
+	return 0;
+}
