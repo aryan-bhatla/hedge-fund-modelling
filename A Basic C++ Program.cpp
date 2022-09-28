a4d1db84ab939b25fa25b6ba2454e3add000c6df
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-09-28 15:25:07 +1000

    Add files via upload

---
+++ A Basic C++ Program.cpp
@@ -0,0 +1,18 @@
+﻿// A Basic C++ Program.cpp : Defines the entry point for the application.
+//
+
+#include "A Basic C++ Program.h"
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	SetConsoleTitle("Console Ouput");  // Set title of console window 
+
+	cout << "\n" << "Hedge Fund Modelling and Analysis: An Object Oriented Approach Using C++"; 
+
+	cin.get(); // Pause console window 
+	return 0; // Return null integer and exit 
+}
