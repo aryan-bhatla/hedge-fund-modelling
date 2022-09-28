302383de7d34f84ebf0b935f4d602cc89d2346da
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-09-28 15:30:33 +1000

    Add files via upload

---
+++ While Loop.cpp
@@ -0,0 +1,22 @@
+﻿// While Loop.cpp : Defines the entry point for the application.
+//
+
+#include "While Loop.h"
+#include "windows.h"
+#include "string.h"
+
+using namespace std;
+
+int main()
+{
+	int n; 
+
+	cout << "\n" << "Enter countdown number: "; 
+
+	while (n > 0) 
+	{
+		
+	}
+
+	return 0;
+}
