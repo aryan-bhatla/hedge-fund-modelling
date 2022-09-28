8618d368d43be33542fe0b58bd4a2f9468253161
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-09-28 15:25:33 +1000

    Add files via upload

---
+++ String Variables.cpp
@@ -0,0 +1,27 @@
+﻿// String Variables.cpp : Defines the entry point for the application.
+//
+
+#include "String Variables.h"
+#include <Windows.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleTitle("Console Ouput"); // Set title of console window 
+
+	// declaring two string variables 
+	string strFirstName = "Aryan";
+	string strLastName = "Bhatla";
+
+	// concatenate the two strings 
+	string strFullName = strFirstName + " " + strLastName;
+
+	cout << "\n" << strFullName; 
+
+	cin.get(); // Pause console window
+
+	return 0;
+}
