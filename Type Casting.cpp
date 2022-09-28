f4da6ae6555689c6d89a2a966ed6bfc60fcba0b1
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-09-28 15:26:25 +1000

    Add files via upload

---
+++ Type Casting.cpp
@@ -0,0 +1,22 @@
+﻿// Type Casting.cpp : Defines the entry point for the application.
+//
+
+#include "Type Casting.h"
+#include "Windows.h"
+#include "string"
+
+using namespace std;
+
+int main()
+{
+	// declare variables 
+	int a = 6; 
+	int b = 4; 
+
+	cout << "\n " << a / b << "\n"; // Integer division 
+	cout << " " << (double)a / b << "\n "; // Type casting to integer division 
+
+	cin.get(); // Pause console window
+
+	return 0;
+}
