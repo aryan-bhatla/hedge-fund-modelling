3147f1324a48cdecba8c3b2cf5ad4e4a43b687f1
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-09-28 15:28:31 +1000

    Add files via upload

---
+++ Input and Output.cpp
@@ -0,0 +1,28 @@
+﻿// Input and Output.cpp : Defines the entry point for the application.
+//
+
+#include "Input and Output.h"
+#include "windows.h" 
+#include "string.h"
+
+using namespace std;
+
+int main()
+{
+	
+	// Declare variable 
+	int n;
+
+	// Get value from keyboard 
+	cout << "\n" << "Enter number of data point: ";
+
+	// Assign value to n
+	cin >> n; 
+
+	// Output result 
+	cout << " " << "You entered " << n << "\n";
+
+	system("PAUSE"); // pause console window 
+
+	return 0;
+}
