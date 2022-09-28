f4b10d83685b0b662b1002cbe6e3d85eebec8828
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-09-28 15:25:58 +1000

    Add files via upload

---
+++ Declaring Variables.cpp
@@ -0,0 +1,34 @@
+﻿// Declaring Variables.cpp : Defines the entry point for the application.
+//
+
+#include "Declaring Variables.h"
+#include <Windows.h>
+#include <iostream> 
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleTitle("Console Output"); // Set title of console window 
+
+	// declare variables 
+	int x, y; 
+	int result; 
+
+	// assign values 
+	x = 4;
+	y = 2; 
+	x = x + 1; 
+
+	// do something 
+	result = x - y; 
+
+	cout << "\n " << result << "\n "; 
+
+	cin.get(); // Pause console window
+
+	return 0;
+}
+
+
