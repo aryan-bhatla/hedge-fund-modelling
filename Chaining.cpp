3cce6dd393efc6afd0b64e7669a975b492157478
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-09-28 15:28:56 +1000

    Add files via upload

---
+++ Chaining.cpp
@@ -0,0 +1,27 @@
+﻿// Chaining.cpp : Defines the entry point for the application.
+//
+
+#include "Chaining.h"
+#include "windows.h"
+#include "string.h"
+
+using namespace std;
+
+int main()
+{
+	// Declare variables 
+	int a; 
+	int b; 
+
+	// Get values from keyboard 
+	cout << "\n " << "Enter values from keyboard: "; 
+
+	// Assign values to a and b using chaining 
+	cin >> a >> b; 
+
+	cout << " " << "The two values were " << a << " and " << b << "\n"; 
+	
+	system("PAUSE");
+
+	return 0;
+}
