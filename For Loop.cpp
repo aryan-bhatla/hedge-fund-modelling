477fa14a5ca1d78c22eac90a6af1e70381732447
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-09-28 15:30:13 +1000

    Add files via upload

---
+++ For Loop.cpp
@@ -0,0 +1,21 @@
+﻿// For Loop.cpp : Defines the entry point for the application.
+//
+
+#include "For Loop.h"
+#include "windows.h"
+#include "string.h"
+
+using namespace std;
+
+int main()
+{ 
+	// For loop 
+	for (int i = 3; i > 0; i--)
+	{
+		cout << " " << i << ",";
+	}
+
+	cout << " " << "Go" << "\n";
+
+	return 0;
+}
