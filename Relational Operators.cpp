6bc7763d0efee9ec4ec444662d4ce17671e08ae1
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-09-28 15:27:58 +1000

    Add files via upload

---
+++ Relational Operators.cpp
@@ -0,0 +1,20 @@
+﻿// Relational Operators.cpp : Defines the entry point for the application.
+//
+
+#include "Relational Operators.h"
+#include "Windows.h"
+#include "string"
+
+using namespace std;
+
+int main()
+{
+	cout << "\n " << "4 is greater than 3 = " <<  (4 > 3) << "\n";
+	cout << "\n " << "4 is less than 3 = " <<  (4 < 3) << "\n";
+	cout << "\n " << "4 is equal to 3 = "  << (4 == 3) << "\n";
+	cout << "\n " << "4 is not equal to 3 = " << (4 != 3) << "\n";
+
+	cin.get(); // pause console window 
+
+	return 0;
+}
