f19390cde2eaad2fb81bb5c4f46226bc6da66c3b
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-09-28 15:27:39 +1000

    Add files via upload

---
+++ Modulo Operators.cpp
@@ -0,0 +1,21 @@
+﻿// Modulo Operators.cpp : Defines the entry point for the application.
+//
+
+#include "Modulo Operators.h"
+#include "Windows.h"
+#include "string"
+
+using namespace std;
+
+int main()
+{
+	// declare variables 
+	int x = 11; 
+	int y = 3; 
+
+	cout << "\n " << "Remainder of 11 divided by 3 = " << x % y << "\n ";
+
+	cin.get(); // pause console window 
+
+	return 0;
+}
