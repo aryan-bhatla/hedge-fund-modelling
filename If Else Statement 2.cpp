62a392c786593624d9fd4d143a195733cfcb857f
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-09-28 15:29:33 +1000

    Add files via upload

---
+++ If Else Statement 2.cpp
@@ -0,0 +1,32 @@
+﻿// If Else Statement 2.cpp : Defines the entry point for the application.
+//
+
+#include "If Else Statement 2.h"
+#include "windows.h"
+#include "string.h"
+
+using namespace std;
+
+int main()
+{
+	int a;
+
+	cout << "\n" << "Enter a positive or negative number: ";
+
+	cin >> a;
+
+	// If else statement 
+	if (a > 0)
+	{
+		cout << " " << "The entered number was positive" << "\n";
+	}
+	else if (a < 0)
+	{
+		cout << " " << "The entered number was negative" << "\n";
+	}
+	else
+	{
+		cout << " " << "The entered number was zero" << "\n";
+	}
+	return 0;
+}
