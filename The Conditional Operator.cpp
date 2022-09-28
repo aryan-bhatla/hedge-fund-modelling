cd60bdb65f7a451089b78b7eb3e48d740e8071e0
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-09-28 15:28:14 +1000

    Add files via upload

---
+++ The Conditional Operator.cpp
@@ -0,0 +1,29 @@
+﻿// The Conditional Operator.cpp : Defines the entry point for the application.
+//
+
+#include "The Conditional Operator.h"
+#include "Windows.h"
+#include "string"
+
+using namespace std;
+
+int main()
+{
+	// declare variables 
+	int a;
+	int b; 
+	int c;
+
+	// assign values to a and b 
+	a = 4; 
+	b = 6; 
+
+	// use conditional expression 
+	c = (a > b) ? a : b;
+
+	cout << "\n" << "Conditional expression gives " << c << "\n ";
+
+	cin.get(); // Pause console window 
+
+	return 0;
+}
