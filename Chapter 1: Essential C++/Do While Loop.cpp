a8ea35e3ebe96b58dadd1fada3ff3ee62d0ef0c5
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-09-29 18:39:47 +1000

    Add files via upload

---
+++ Chapter 1: Essential C++/Do While Loop.cpp
@@ -0,0 +1,28 @@
+﻿// Do While Loop.cpp : Defines the entry point for the application.
+//
+
+#include "Do While Loop.h"
+#include "windows.h"
+#include "string"
+
+using namespace std;
+
+int main()
+{
+	int n; 
+
+	// Do While Loop
+	do
+	{
+		cout << "\n " << "Enter number (0 to end loop): ";
+		cin >> n; 
+		cout << " " << "The number was " << n << "\n ";
+	}
+
+	while (n != 0);
+		cout << "0 ends the loop" << "\n ";
+	
+		system("PAUSE"); // pause console window 
+
+	return 0;
+}
