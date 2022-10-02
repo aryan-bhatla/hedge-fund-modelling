0d8e6f017f80fa99c2b0677551342eb4e8d55138
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-10-02 17:07:05 +1100

    Add files via upload

---
+++ Chapter 1: Essential C++/Call by Value.cpp
@@ -0,0 +1,41 @@
+﻿// Call by Value.cpp : Defines the entry point for the application.
+//
+
+#include "Call by Value.h"
+#include "windows.h"
+#include "string"
+#include "math.h"
+
+using namespace std;
+
+// Call by value implies that copies of any variables within the parameter list are made as they are called 
+// Inefficient when dealing with a large number of calls 
+
+// Function prototype 
+void swap(int i, int j);
+
+int main()
+{
+	// Declare variables
+
+	int i = 1; 
+	int j = 2; 
+
+	cout << "\n " << "i = " << i << "\n " << "j = " << j << "\n "; 
+	swap(i, j);
+	cout << "i = " << i << "\n " << "j = " << j << "\n "; 
+	
+	cin.get();
+	
+	return 0;
+}
+
+
+// Function definition 
+void swap(int i, int j)
+{
+	int t; 
+	t = i;
+	i = j;
+	j = t;
+}
