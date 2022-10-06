78627297b1569fa631121a00950627f5ef9f8b4d
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-10-06 17:02:56 +1100

    Add files via upload

---
+++ Chapter 1: Essential C++/A Simple Implementation of a Class.cpp
@@ -0,0 +1,43 @@
+﻿// A Simple Implementation of a Class.cpp : Defines the entry point for the application.
+//
+
+#include "A Simple Implementation of a Class.h"
+#include <iostream> 
+#include <windows.h>
+
+
+using namespace std;
+
+// Default Constructor 
+BasicClass::BasicClass()
+{
+}
+
+// Default Destructor 
+BasicClass::~BasicClass()
+{
+}
+
+void BasicClass::sayHello()
+{
+	cout << " Hello\n";
+}
+
+void BasicClass::sayBye()
+{
+	cout << " Bye\n";
+}
+
+int main()
+{
+	// Create an instance of the BasicClass class 
+	BasicClass obj;
+
+	// Call the sayHello and sayBye functions using the dot operator 
+	obj.sayHello();
+	obj.sayBye();
+
+	cin.get();
+
+	return 0;
+}
---
+++ Chapter 1: Essential C++/A Simple Implementation of a Class.h
@@ -0,0 +1,21 @@
+﻿// A Simple Implementation of a Class.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+class BasicClass
+{
+public:
+	BasicClass();
+	virtual ~BasicClass();
+	void sayHello();
+	void sayBye();
+
+private:
+};
+
+
+
+// TODO: Reference additional headers your program requires here.
