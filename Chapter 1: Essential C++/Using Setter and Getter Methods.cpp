7b7570a92d9f362f0907592ed2da11fea39ff4a6
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-10-10 17:41:14 +1100

    Add files via upload

---
+++ Chapter 1: Essential C++/Using Setter and Getter Methods.cpp
@@ -0,0 +1,46 @@
+﻿// Using Setter and Getter Methods.cpp : Defines the entry point for the application.
+//
+
+#include "Using Setter and Getter Methods.h"
+#include <iostream>
+#include <windows.h>
+using std::cout;
+using std::cin;
+
+// Default constructor
+AClass::AClass()
+{
+	//cout << " Default constructor called ...\n";
+}
+
+// Default destructor
+AClass::~AClass()
+{
+	//cout << " Default destructor called ...\n";
+}
+
+// Setter function
+void AClass::setName(string name)
+{
+	m_name = name;
+}
+
+// Getter function with const modifier
+string AClass::getName() const
+{
+	return m_name;
+}
+
+
+int main ()
+{
+	// Create an instance of the AClass class (instantiation)
+	AClass obj;
+
+	// Call the sayHello() funtion using the dot operator
+	obj.setName("Aryan");
+	cout << '\n' << obj.getName() << '\n'; // Call getter using dot operator
+
+	return 0;
+}
+
---
+++ Chapter 1: Essential C++/Using Setter and Getter Methods.h
@@ -0,0 +1,23 @@
+﻿// Using Setter and Getter Methods.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+using std::string;
+
+class AClass
+{
+public:
+	AClass(); // Default constructor
+	void setName(string x); // Setter prototype
+	string getName() const; // Getter prototype with const modifier
+	virtual ~AClass();// Default destructor
+
+private:
+	string m_name; // Member variable
+};
+
+
+// TODO: Reference additional headers your program requires here.
