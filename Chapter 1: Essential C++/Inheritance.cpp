63c619514c4c69ef0b0c25d60299411515ce859a
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-10-13 22:44:05 +1100

    Add files via upload

---
+++ Chapter 1: Essential C++/Inheritance.cpp
@@ -0,0 +1,42 @@
+﻿// Inheritance.cpp : Defines the entry point for the application.
+//
+
+#include "Inheritance.h"
+
+using namespace std;
+
+Mother::Mother()
+{
+}
+
+void Mother::sayName()
+{
+	cout << "I am part of the Bhatla family" << "\n ";
+}
+
+Mother::~Mother()
+{
+}
+
+Daughter::Daughter()
+{
+}
+
+Daughter::~Daughter()
+{
+}
+
+int main()
+{
+	Mother mum;
+	cout << "\n " << "Mother" << "\n ";
+	mum.sayName();
+
+	Daughter none; 
+	cout << "\n " << "Daughter" << "\n ";
+	none.sayName();
+
+	cin.get();
+
+	return 0;
+}
---
+++ Chapter 1: Essential C++/Inheritance.h
@@ -0,0 +1,24 @@
+﻿// Inheritance.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+class Mother
+{
+public:
+	Mother();
+	void sayName();
+
+	virtual ~Mother();
+};
+
+class Daughter : public Mother
+{
+public:
+	Daughter(); 
+
+	virtual ~Daughter();
+};
+// TODO: Reference additional headers your program requires here.
