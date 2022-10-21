8569545d63cb192d8541d48c3db509378fe10f0a
Aryan Bhatla <67987363+aryan-bhatla@users.noreply.github.com>
2022-10-21 21:02:15 +1100

    Add files via upload

---
+++ Chapter 4: Statistical Analysis/Skeleton of Stats Class/Stats Class Skeleton Header.h
@@ -0,0 +1,17 @@
+﻿// Stats Class Skeleton.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+
+class Stats
+{
+public:
+	Stats() {}
+	virtual ~Stats() {}
+
+private:
+};
+// TODO: Reference additional headers your program requires here.
